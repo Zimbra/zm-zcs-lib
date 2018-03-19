b6c533a301259c6f847a97b3029f1f4ead52fb61
Rupali Desai <rupali.desai@synacor.com>
2018-03-19 09:46:49 +0530

    ZCS-4355 renaming 2 jars in lib/jars

--- pkg-builder.pl
+++ pkg-builder.pl
@@ -211,8 +211,8 @@ sub stage_zimbra_core_lib($)
         cpy_file("build/dist/ehcache-3.1.2.jar",                                    "$stage_base_dir/opt/zimbra/lib/jars/ehcache-3.1.2.jar");
         cpy_file("build/dist/zimbra-charset.jar",                                   "$stage_base_dir/opt/zimbra/lib/jars/zimbra-charset.jar");
         cpy_file("build/dist/ant-1.6.5.jar",                                        "$stage_base_dir/opt/zimbra/lib/jars/ant-1.6.5.jar");
-        cpy_file("build/dist/json-20090211.jar",                                    "$stage_base_dir/opt/zimbra/lib/jars/json-20090211.jar");
-        cpy_file("build/dist/commons-logging-1.1.1.jar",                            "$stage_base_dir/opt/zimbra/lib/jars/commons-logging-1.1.1.jar");
+        cpy_file("build/dist/json-20090211.jar",                                    "$stage_base_dir/opt/zimbra/lib/jars/json.jar");
+        cpy_file("build/dist/commons-logging-1.1.1.jar",                            "$stage_base_dir/opt/zimbra/lib/jars/commons-logging.jar");
 
    
    return ["."];
