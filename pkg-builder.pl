4de17de79461f9b382c0f06c5d3b142d36273fbb
Sneha Patil <sneha.patil@synacor.com>
2019-07-04 23:06:18 +0530

    ZCS-7498:Creating signature through ZCO puts extra space within sign

--- pkg-builder.pl
+++ pkg-builder.pl
@@ -191,7 +191,12 @@ sub stage_zimbra_core_lib($)
         cpy_file("build/dist/neethi-3.0.2.jar",                                     "$stage_base_dir/opt/zimbra/lib/jars/neethi-3.0.2.jar");
         cpy_file("build/dist/nekohtml-1.9.13.1z.jar",                               "$stage_base_dir/opt/zimbra/lib/jars/nekohtml-1.9.13.1z.jar");
         cpy_file("build/dist/oauth-20100527.jar",                                   "$stage_base_dir/opt/zimbra/lib/jars/oauth-20100527.jar");
-        cpy_file("build/dist/owasp-java-html-sanitizer-20190503.1.jar",                   "$stage_base_dir/opt/zimbra/lib/jars/owasp-java-html-sanitizer-r239.jar");
+        cpy_file("build/dist/owasp-java-html-sanitizer-r239.jar",                   "$stage_base_dir/opt/zimbra/lib/jars/owasp-java-html-sanitizer-r239.jar");
+        cpy_file("build/dist/antisamy-1.5.3.jar",                                   "$stage_base_dir/opt/zimbra/lib/jars/antisamy-1.5.3.jar");
+        cpy_file("build/dist/batik-css-1.7.jar",                                    "$stage_base_dir/opt/zimbra/lib/jars/batik-css-1.7.jar");
+        cpy_file("build/dist/batik-i18n-1.9.jar",                                   "$stage_base_dir/opt/zimbra/lib/jars/batik-i18n-1.9.jar");
+        cpy_file("build/dist/batik-util-1.8.jar",                                   "$stage_base_dir/opt/zimbra/lib/jars/batik-util-1.8.jar");
+        cpy_file("build/dist/sac-1.3.jar",                                          "$stage_base_dir/opt/zimbra/lib/jars/sac-1.3.jar");
         cpy_file("build/dist/policy-2.3.jar",                                       "$stage_base_dir/opt/zimbra/lib/jars/policy-2.3.jar");
         cpy_file("build/dist/slf4j-api-1.6.4.jar",                                  "$stage_base_dir/opt/zimbra/lib/jars/slf4j-api-1.6.4.jar");
         cpy_file("build/dist/slf4j-log4j12-1.6.4.jar",                              "$stage_base_dir/opt/zimbra/lib/jars/slf4j-log4j12-1.6.4.jar");
