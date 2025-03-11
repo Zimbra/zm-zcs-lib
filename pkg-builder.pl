f84684c7abd7ba2681e64499bf54719c764565e1
“Chetanlanje” <chetanlanje74@gmail.com>
2025-03-11 15:28:49 +0000

    ZBUG-4023:Upgraded jetty to 9.4.57.v20241219

--- pkg-builder.pl
+++ pkg-builder.pl
@@ -228,7 +228,7 @@ sub stage_zimbra_core_lib($)
         cpy_file("build/dist/activation-1.1.1.jar",                                 "$stage_base_dir/opt/zimbra/lib/jars/activation-1.1.1.jar");
         cpy_file("build/dist/istack-commons-runtime-3.0.8.jar",                     "$stage_base_dir/opt/zimbra/lib/jars/istack-commons-runtime-3.0.8.jar");
         cpy_file("build/dist/javax.annotation-api-1.2.jar",                         "$stage_base_dir/opt/zimbra/lib/jars/javax.annotation-api-1.2.jar");
-        cpy_file("build/dist/apache-jsp-9.4.46.v20220331.jar",                      "$stage_base_dir/opt/zimbra/lib/jars/apache-jsp-9.4.46.v20220331.jar");
+        cpy_file("build/dist/apache-jsp-9.4.57.v20241219.jar",                      "$stage_base_dir/opt/zimbra/lib/jars/apache-jsp-9.4.57.v20241219.jar");
         cpy_file("build/dist/UserAgentUtils-1.21.jar",                              "$stage_base_dir/opt/zimbra/lib/jars/UserAgentUtils-1.21.jar");
         cpy_file("build/dist/tika-core-1.24.1.jar",                                 "$stage_base_dir/opt/zimbra/lib/jars/tika-core-1.24.1.jar");
 	cpy_file("build/dist/apache-mime4j-core-0.8.7.jar",                         "$stage_base_dir/opt/zimbra/lib/jars/apache-mime4j-core-0.8.7.jar");
